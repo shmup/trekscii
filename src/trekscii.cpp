992a8d456ba4e0ec5b74dfb49f70492b34e404b3
Jared Miller <jared@smell.flowers>
2024-01-04 00:18:32 -0500

    Set window width and height if not specified

--- src/trekscii.cpp
+++ src/trekscii.cpp
@@ -1,5 +1,7 @@
 #include "trekscii.h"
 #include <iostream>
+#include <sys/ioctl.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
 
@@ -10,8 +12,18 @@ int main(int argc, char **argv) {
   fclose(devrnd);
   srand(seed);
 
-  int dimX = std::stoi(argv[1]);
-  int dimY = std::stoi(argv[2]);
+  int dimX, dimY;
+
+  if (argc == 3) {
+    dimX = std::stoi(argv[1]);
+    dimY = std::stoi(argv[2]);
+  } else {
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+
+    dimX = w.ws_col;
+    dimY = w.ws_row;
+  }
 
   Canvas canvas = Canvas(dimX, dimY - 2);
   canvas.generate();
