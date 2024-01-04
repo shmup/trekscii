b1e3b6f8a0311899a4afe6680b8081d7b36f0392
Jared Miller <jared@smell.flowers>
2024-01-04 00:11:42 -0500

    Run files through clang-format

--- src/trekscii.cpp
+++ src/trekscii.cpp
@@ -1,22 +1,19 @@
-#include <iostream>
 #include "trekscii.h"
+#include <iostream>
 
+int main(int argc, char **argv) {
 
+  // random seeding copied from stackoverflow :)
+  uint32_t seed = 0;
+  FILE *devrnd = fopen("/dev/random", "r");
+  fread(&seed, 4, 1, devrnd);
+  fclose(devrnd);
+  srand(seed);
 
-int main(int argc, char** argv) {
+  int dimY = std::stoi(argv[1]);
+  int dimX = std::stoi(argv[2]);
 
-    // random seeding copied from stackoverflow :)
-    uint32_t seed=0;
-    FILE *devrnd = fopen("/dev/random","r");
-    fread(&seed, 4, 1, devrnd);
-    fclose(devrnd);
-    srand(seed);
-    
-
-    int dimY = std::stoi(argv[1]);
-    int dimX = std::stoi(argv[2]);
-
-    Canvas canvas = Canvas(dimX, dimY - 2);
-    canvas.generate();
-    canvas.print();
+  Canvas canvas = Canvas(dimX, dimY - 2);
+  canvas.generate();
+  canvas.print();
 }
--- src/trekscii.h
+++ src/trekscii.h
@@ -1,60 +1,56 @@
 #pragma once
 
-#include <iostream>
-#include <vector>
-#include <string>
-#include <cmath>
-#include <random>
 #include <algorithm>
 #include <assert.h>
-
-
+#include <cmath>
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
 
 const extern std::vector<std::string> SHIP_ART;
 const extern std::vector<std::string> SPACE_ART;
 const extern std::vector<char> STAR_CHARS;
 const extern std::vector<std::string> COLORSET;
 
+struct RenderConfig {
+  const static int PADDING = 1;
+  const static int HORIZ_PLACEMENT_PADDING = 4;
+  const static int VERT_PLACEMENT_PADDING = 2;
 
-struct RenderConfig
-{
-    const static int PADDING = 1;
-    const static int HORIZ_PLACEMENT_PADDING = 4;
-    const static int VERT_PLACEMENT_PADDING = 2;
+  constexpr static double VIGNETTE = 0.25;
 
-    constexpr static double VIGNETTE = 0.25;
+  constexpr static double CLUSTER_RATIO = 1.0 / 2000.0;
 
-    constexpr static double CLUSTER_RATIO = 1.0 / 2000.0;
-
-    const static std::string ENDC;
+  const static std::string ENDC;
 };
-
 
 // Global util funcs
 std::vector<std::vector<char>> LiteralToMat(std::string);
-size_t Squarify(std::vector<std::vector<char>>&);
-void Reverse(std::vector<std::vector<char>>&);
-void Overlay(std::vector<std::vector<std::string>>& base, const std::vector<std::vector<char>>& overlay, int x, int y, std::string color = RenderConfig::ENDC);
+size_t Squarify(std::vector<std::vector<char>> &);
+void Reverse(std::vector<std::vector<char>> &);
+void Overlay(std::vector<std::vector<std::string>> &base,
+             const std::vector<std::vector<char>> &overlay, int x, int y,
+             std::string color = RenderConfig::ENDC);
 size_t GetManhattanDist(size_t x1, size_t y1, size_t x2, size_t y2);
 
-class Canvas
-{
+class Canvas {
 public:
-    Canvas(size_t w, size_t h);
+  Canvas(size_t w, size_t h);
 
-    void generate();
-    void print();
+  void generate();
+  void print();
 
 private:
-    std::vector<std::vector<char>> starfield;
-    std::vector<std::vector<std::string>> render;
+  std::vector<std::vector<char>> starfield;
+  std::vector<std::vector<std::string>> render;
 
-    std::vector<std::pair<size_t, size_t>> clusterCenters;
+  std::vector<std::pair<size_t, size_t>> clusterCenters;
 
-    void getClusterCenters();
-    void spawnStars();
-    void renderStars();
-    void overlayArt();
+  void getClusterCenters();
+  void spawnStars();
+  void renderStars();
+  void overlayArt();
 
-    size_t w, h, area;
+  size_t w, h, area;
 };
